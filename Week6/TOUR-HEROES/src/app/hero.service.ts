ddbbb43a2b44be86e8034519c1ded0bba771e21f
DamiNo007 <dmoldabayev.99@gmail.com>
2020-04-16 12:19:42 +0600

    test

--- Week6/TOUR-HEROES/src/app/hero.service.ts
+++ Week6/TOUR-HEROES/src/app/hero.service.ts
@@ -77,6 +77,8 @@ export class HeroService {
     );
   }
 
+  
+
   /** DELETE: delete the hero from the server */
   deleteHero (hero: Hero | number): Observable<Hero> {
     const id = typeof hero === 'number' ? hero : hero.id;
